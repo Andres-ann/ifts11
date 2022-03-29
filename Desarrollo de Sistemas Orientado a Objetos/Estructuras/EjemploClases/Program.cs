6ecacc645baa27e38aa0be40bdd8b4fe13096759
Andrés Negrette <80859408+Andres-ann@users.noreply.github.com>
2022-03-28 21:31:02 -0300

    first commit

---
+++ Desarrollo de Sistemas Orientado a Objetos/Estructuras/EjemploClases/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Ejemplos_Clases
+{
+
+    struct Persona
+    {
+        /*
+        Por ejemplo en el siguiente ejemplo se define una estructura para guardar cierta informacion relativa a las personas, por ejemplo para ser utilizadas en un instituto, en un sistema de
+        gestion de turnos, etc.
+        */
+        string nombre;
+        string apellido;
+        int edad;
+        string dni;
+    };
+
+    /*
+    Para una aplicacion matematica que realice calculos con numeros complejos y operaciones sobres objetos geometricos podria definir 
+    */
+    struct NComplejo
+    {
+        int real;
+        int imaginario;
+    };
+    struct Punto
+    {
+        int x;
+        int y;
+    };
+    struct Circulo
+    {
+        Punto centro; // observar como una estructura puede incluir otras.
+        int radio;
+    };
+    struct Rectangulo
+    {
+        int x1, y1;
+        int x2, y2;
+        int x3, y3;
+        int x4, y4;
+    };
+    // otra forma incluyendo otras estructuras.
+    struct RectanguloV2
+    {
+        Punto v1, v2, v3, v4;
+    };
+
+    /*
+    En el siguiente ejemplo podria ser una estructura definida dentro de un programa que
+    maneje las ventas de un local, en particular en este caso los datos de un cliente.
+    */
+    struct Fecha
+    {
+        short dia;
+        short mes;
+        short anio;
+    };
+    struct Cliente
+    {
+        Persona datosBasicos;
+        float credito;
+        float descuento;
+        string preferencias;
+        Fecha ultimaCompra;
+        string opiniones;
+        string email;
+    };
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Desarrollo de Sistemas Orientado a Objetos/Estructuras/EjemploLibros/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace source
+{
+    class Program
+    {
+        public struct tLibro
+        {
+            public string titulo;
+            public string autor;
+            public string editorial;
+            public int anio;
+        };
+        static void Main(string[] args)
+        {
+            var libroPreferido = new tLibro
+            {
+                titulo = "El mitico hombre-mes",
+                autor = "Frederik P. Brooks, Jr",
+                editorial = "Universidad de Carolina del Norte",
+                anio = 1975
+            };
+            var otroLibro = new tLibro
+            {
+                titulo = "La clave es la servilleta",
+                autor = "Dan Roam",
+                editorial = "Editorial Norma",
+                anio = 2009
+            };
+            Console.WriteLine("Libro Preferido");
+            Console.WriteLine("Titulo del libro:" + libroPreferido.titulo);
+            Console.WriteLine("Autor del libro: " + libroPreferido.autor);
+            Console.WriteLine("Editado por " + libroPreferido.editorial);
+            Console.WriteLine("A~no de Edicion:" + libroPreferido.anio);
+            Console.WriteLine();
+            Console.WriteLine("Otro buen libro");
+            Console.WriteLine("Titulo del libro:" + otroLibro.titulo);
+            Console.WriteLine("Autor del libro: " + otroLibro.autor);
+            Console.WriteLine("Editado por " + otroLibro.editorial);
+            Console.WriteLine("A~no de Edicion:" + otroLibro.anio);
+        }
+    }
+}
